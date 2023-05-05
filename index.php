0516e89e6b489fb9d7aca445f57de542af140250
AlessandroLembo <alessandrolembo2@gmail.com>
2023-05-05 17:11:33 +0200

    add class wrapper

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
 <body>
     <div class="container">
         <h1 class="text-center mt-5">Scegli ciò che ti serve per il tuo animale domestico!</h1>
-        <div class="row">
+        <div class="row wrapper">
             <h2 class="text-center fs-1 text-uppercase mt-5">Food category</h2>
             <?php foreach ($foods as $food) include __DIR__ . '/models/card/Food_card.php'; ?>
 
         </div>
 
-        <div class="row">
+        <div class="row wrapper">
             <h2 class="text-center fs-1 text-uppercase mt-5">Games category</h2>
             <?php foreach ($games as $game) include __DIR__ . '/models/card/Game_card.php'; ?>
 
         </div>
 
-        <div class="row">
+        <div class="row wrapper">
             <h2 class="text-center fs-1 text-uppercase mt-5">Kennel category</h2>
             <?php foreach ($kennels as $kennel) include __DIR__ . '/models/card/Kennel_card.php'; ?>
 
